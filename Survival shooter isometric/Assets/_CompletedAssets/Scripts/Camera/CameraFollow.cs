97c118ee88acc4887e9424ead385bf2e9359e8e2
BenjaminHerd <benoherdo@gmail.com>
2017-03-22 13:16:46 +1000

    Added in files for week 5 submission
    Demonstration followed to step 3: camera control.

---
+++ Survival shooter isometric/Assets/_CompletedAssets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f; //Adds some lag to make smoother transitions, 5f is the value for it
+
+    Vector3 offset; //store distance for offset for each update
+
+    void Start()
+    {
+        offset = transform.position - target.position; //difference from camera postion to player postion
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset; // target camera position which is the camera postion plus its offset
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime); //Moves towards target postion and how fast to do this
+    }
+}
